e607017944c3a837832a27320a6c93408800471d
Moti Singh <motisinghg5@gmail.com>
2021-01-17 14:06:04 +0530

    ful2-moti'

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -9,5 +9,6 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("application start");
+		System.out.println("we are in the spring boot application");
 	}
 }
